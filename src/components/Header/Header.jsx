fdae6c2efd808e2539b5164e8e202c62120fbb14
biksrek <biks15216@gmail.com>
2025-11-28 20:02:40 +0530

    Header and herso section 2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from "./Header.module.css"
+import Button from "../Button.jsx/Button";
 export default function Header(){
     const [open,setOpen]=useState(false);
-    return <header className={styles.header}>
+    const[scrolled,setScrolled]=useState(false);
+    useEffect(()=>{
+    function handleScrolled (){
+        setScrolled(window.scrollY>50)
+    }
+    window.addEventListener("scroll",handleScrolled)
+    return ()=>{
+        window.removeEventListener("scroll",handleScrolled)
+    }
+    })
+
+    return <header className={`${styles.header} ${scrolled ? styles.scroll :""}`}>
         <div className={styles.logo}>My Website</div>
         <div className={styles.hamburger} onClick={() => setOpen(!open)}>
         ☰
         </div>
         <nav className={`${styles.nav} ${open ? styles.open: ''}`}>
             <ul className="">
-                <li><a href="/">Packages</a></li>
-                <li><a href="/">Packages</a></li>
-                <li><a href="/">Packages</a></li>
-                <li><a href="/">Packages</a></li>
-                <li><a href="/">Packages</a></li>
-                <li><a href="/">Packages</a></li>
+                <li><a href="#">Packages</a></li>
+                <li><a href="#">Custom Tours</a></li>
+                <li><a href="#">Destinations</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Theme</a></li>
+                <li><Button text="Book Now" onClick={()=>{}}/></li>
             </ul>
         </nav>
     </header>
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,6 +1,14 @@
+import Button from "../Button.jsx/Button";
 import styles from "./HeroSection.module.css";
 export default function HeroSection(){
     return <section className={styles.hero}>
         <div className={styles.overlay}></div>
+        <div>
+            <h1>Discover the <br/>Majestic Himalayas</h1>
+            <p>Experience the breathtaking beauty of Darjeeling <br/>and Sikkim with our expertly crafted tour packages. <br/>From tea gardens to mountain peaks,<br/> create memories that last a lifetime.</p>
+            <Button text="Explore Packages" onClick={()=>{}}/>
+            <Button text="Watch Video >" onClick={()=>{}}/>
+        </div>
+        
     </section>
 }
